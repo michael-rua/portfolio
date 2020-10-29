95be83006dbd41617064fcc4f74689c7c51b9584
michael-rua <michaelrua1@gmail.com>
2020-10-29 19:24:09 +1300

    add responsive titles

--- src/VideoHero.jsx
+++ src/VideoHero.jsx
@@ -18,7 +18,7 @@ export default function VideoHero () {
     <div className='videoHero'> 
       <div className='titles'>
         <h1 className={style}>Michael Rua</h1>
-        <h2 style= {{fontSize: '2rem', top: '54%', order: '2'}} className={style}>Software Developer | Pilot</h2>
+        <h2 style= {{fontSize: '2rem', top: '58%', order: '2'}} className={style}>Software Developer | Pilot</h2>
       </div>
       <video playsInline autoPlay muted loop poster='/poster.png'id="myVideo">
         <source src="/skyBeach-loop (1).mp4" type="video/mp4" />
