91f57df261ffc27fe909517d0833f88c310c83b8
michael-rua <michaelrua1@gmail.com>
2020-11-03 12:59:34 +1300

    add font-awesome icons to footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMobileAlt } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import './styles/section.css'
+
+
+
 export default function Footer () {
   return (
     <div id='footer'>
     <div className='footer content'>
-      
+    <FontAwesomeIcon icon={faGithub} size="lg"/>
+    <FontAwesomeIcon icon={faLinkedin} size="lg"/>
+    <FontAwesomeIcon icon={faMobileAlt} size="lg"/>
+    <FontAwesomeIcon icon={faEnvelope} size="lg"/>
     </div>
     </div>
+    
   )
 }
