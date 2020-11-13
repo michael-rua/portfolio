4a8a15adda37ea7239445446eb50f23f36e3e583
michael-rua <michaelrua1@gmail.com>
2020-11-13 15:35:01 +1300

    add few changes

--- src/About.jsx
+++ src/About.jsx
@@ -13,7 +13,7 @@ export default function About () {
         </div> 
         <div className='right-about-section'>   
           <p>
-           I love cats, outdoors and mountain biking.
+          ada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, 
 
           </p>
         </div>
