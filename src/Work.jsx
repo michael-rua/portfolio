7059cc3d3be92953b29155125613d488d0e83621
michael-rua <michaelrua1@gmail.com>
2020-11-16 13:19:15 +1300

    fix horizontal scroll

--- src/Work.jsx
+++ src/Work.jsx
@@ -17,14 +17,15 @@ export default function Work () {
             <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est </p>
           </div>
           <div className='right-work-side'>
-          <div className='work-img-container'>
-            <img className='work-img' src='/reciplaniphone4.png' alt='app screen images'/>
-            
-            <div class="work-img-overlay">
-              <a className='reciplan-link' target='_blank' rel="noopener noreferrer" href='https://www.reciplan.co.nz'>Go to Reciplan.co.nz</a>
-            </div>
-          </div> 
             <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est </p>
+            <div className='work-img-container'>
+              <img className='work-img' src='/reciplaniphone4.png' alt='app screen images'/>
+            
+              <div class="work-img-overlay">
+                <a className='reciplan-link' target='_blank' rel="noopener noreferrer" href='https://www.reciplan.co.nz'>Go to Reciplan.co.nz</a>
+              </div>
+            </div> 
+            
           </div>
         </div>
       </div>
